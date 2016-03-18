e38faa192cc7d3be08fe630873ff81b1d1cfb250
Le Phuong <notteen@gmail.com>
2016-03-18 12:12:56 +0700

    add condition to check compare date

--- src/assets/js/datetime-compare-validator.js
+++ src/assets/js/datetime-compare-validator.js
@@ -1,44 +1,46 @@
 /**
-* @link https://github.com/nepstor/yii2-datetime-compare-validator
-* @copyright Copyright (c) 2014 Nikita Krytskiy
-* @license http://opensource.org/licenses/BSD-3-Clause
-*/
-yii.validation.datetimecompare = function (value, messages, options) {
-    if (options.skipOnEmpty && yii.validation.isEmpty(value)) {
-        return null;
-    }
-    
-    var dateCompareValue, valid = true;
-    var dateValue = moment(value, options.format).unix();
+ * @link https://github.com/nepstor/yii2-datetime-compare-validator
+ * @copyright Copyright (c) 2014 Nikita Krytskiy
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+yii.validation.datetimecompare = function(value, messages, options) {
+	if(options.skipOnEmpty && yii.validation.isEmpty(value)) {
+		return null;
+	}
 
-    dateCompareValue = options.compareAttribute === undefined ? options.compareValue : $('#' + options.compareAttribute).val();
-    dateCompareValue = moment(dateCompareValue, options.format).unix();
+	var dateCompareValue, valid = true;
+	var dateValue               = moment(value, options.format).unix();
 
-    switch (options.operator) {
-        case '=':
-            valid = dateValue == dateCompareValue;
-            break;
-        case '!=':
-            valid = dateValue != dateCompareValue;
-            break;
-        case '>':
-            valid = dateValue > dateCompareValue;
-            break;
-        case '>=':
-            valid = dateValue >= dateCompareValue;
-            break;
-        case '<':
-            valid = dateValue < dateCompareValue;
-            break;
-        case '<=':
-            valid = dateValue <= dateCompareValue;
-            break;
-        default:
-            valid = false;
-            break;
-    }
+	dateCompareValue = options.compareAttribute === undefined ? options.compareValue : $('#' + options.compareAttribute).val();
+	if(dateCompareValue != '' && dateValue != '') {
+		dateCompareValue = moment(dateCompareValue, options.format).unix();
+		console.log(options.compareAttribute);
+		switch(options.operator) {
+			case '=':
+				valid = dateValue == dateCompareValue;
+				break;
+			case '!=':
+				valid = dateValue != dateCompareValue;
+				break;
+			case '>':
+				valid = dateValue > dateCompareValue;
+				break;
+			case '>=':
+				valid = dateValue >= dateCompareValue;
+				break;
+			case '<':
+				valid = dateValue < dateCompareValue;
+				break;
+			case '<=':
+				valid = dateValue <= dateCompareValue;
+				break;
+			default:
+				valid = false;
+				break;
+		}
+	}
 
-    if (!valid) {
-        yii.validation.addMessage(messages, options.message, value);
-    }
+	if(!valid) {
+		yii.validation.addMessage(messages, options.message, value);
+	}
 };
