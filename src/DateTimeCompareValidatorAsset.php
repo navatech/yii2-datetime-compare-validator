33ea6a2d37e560bc6e8af08be747c2727ebcb09f
Lê Phương <notteen@gmail.com>
2016-05-10 15:58:34 +0700

    change assets

--- src/DateTimeCompareValidatorAsset.php
+++ src/DateTimeCompareValidatorAsset.php
@@ -1,10 +1,9 @@
 <?php
 /**
-* @link https://github.com/nepstor/yii2-datetime-compare-validator
-* @copyright Copyright (c) 2014 Nikita Krytskiy
-* @license http://opensource.org/licenses/BSD-3-Clause
-*/
-
+ * @link      https://github.com/nepstor/yii2-datetime-compare-validator
+ * @copyright Copyright (c) 2014 Nikita Krytskiy
+ * @license   http://opensource.org/licenses/BSD-3-Clause
+ */
 namespace navatech\validators;
 
 use yii\web\AssetBundle;
@@ -13,30 +12,30 @@ use yii\web\AssetBundle;
  * Asset bundle provides the javascript files for client validation.
  * @author Nepstor <nepstor_j@mail.ru>
  */
-class DateTimeCompareValidatorAsset extends AssetBundle
-{
-    /**
-     * @inheritdoc
-     */
-    public $sourcePath = '@vendor/navatech/yii2-datetime-compare-validator/src/assets';
-    
-    /**
-     * @inheritdoc
-     */
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
+class DateTimeCompareValidatorAsset extends AssetBundle {
 
-    /**
-     * @inheritdoc
-     */
-    public $js = [
-        'js/datetime-compare-validator.js',
-    ];
-    
-    /**
-     * @inheritdoc
-     */
-    public $depends = [
-        'yii\validators\ValidationAsset',
-        'navatech\validators\MomentJsAsset',
-    ];
+	/**
+	 * @inheritdoc
+	 */
+	public $sourcePath = '@vendor/navatech/yii2-datetime-compare-validator/src/assets';
+
+	/**
+	 * @inheritdoc
+	 */
+	public $publishOptions = ['forceCopy' => YII_DEBUG];
+
+	/**
+	 * @inheritdoc
+	 */
+	public $js = [
+		'js/datetime-compare-validator.js',
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	public $depends = [
+		'yii\validators\ValidationAsset',
+		'navatech\base\assets\MomentJsAsset',
+	];
 }
